7ce89d4d7e8f2385123e6756d8eaca05ef96fca3
Sandra Fernandz <sandraenmunich@gmail.com>
2021-12-30 09:56:19 +0100

    comment code

--- main.js
+++ main.js
@@ -2,13 +2,38 @@
 
 console.log('>> Ready :)');
 
-//chrome://extensions/
+//chrome://extensions/ is where you upload the browser extension
 
 let myLeads = [];
 
 const input = document.querySelector('.js-input');
-const button = document.querySelector('.js-button');
+const addButton = document.querySelector('.js-addButton');
+const deleteButton = document.querySelector('.js-deleteButton');
 const list = document.querySelector('.js-list');
+
+//localStorage.clear();
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+//if leadsFromLocalStorage is truthy, set myLeads ot its value and call render leads
+
+if (leadsFromLocalStorage) {
+  //leads of former sessions appear when opening our chrome extension
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+// empty localStorage with click event
+
+function handleEmptyLocalStorage() {
+  console.log('emptying in process');
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+}
+
+deleteButton.addEventListener('click', handleEmptyLocalStorage);
+
+// add leads to lead tracker using click event in button
 
 function handleButtonClick() {
   console.log('you clicked me');
@@ -17,16 +42,21 @@ function handleButtonClick() {
   console.log(inputLead);
   console.log(myLeads);
   input.value = '';
+  //Save the myLeads Array to localStorage
+  //PS- remember JSON.stringify
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
   renderLeads();
+  //to verify that it works
+  console.log(localStorage.getItem('myLeads'));
 }
 
-button.addEventListener('click', handleButtonClick);
+addButton.addEventListener('click', handleButtonClick);
 
 function renderLeads() {
   let listItems = '';
   for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i]);
-    listItems += `<li><a href='${myLeads[i]}' target='_blank' class='body-list-link'> ${myLeads[i]} </a></li>`;
+    listItems += `<li><a href='${myLeads[i]}' target='_blank' class='list-link'> ${myLeads[i]} </a></li>`;
   }
   list.innerHTML = listItems;
 }
